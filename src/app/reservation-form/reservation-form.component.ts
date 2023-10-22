498d6575970b06e7fa3318f78837c7c4a603acd9
sadatmalik <sadat.malik@me.com>
2023-10-22 13:42:25 +0100

    add controls to form group with initial values and validators. Validate form on submit.

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
@@ -6,11 +6,27 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
 
   reservationForm: FormGroup = new FormGroup<any>({});
 
-  onSubmit() {
-
+  constructor(private formBuilder: FormBuilder) {
   }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required], // initial value, required
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log("valid");
+    }
+  }
+
 }
