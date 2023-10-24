a2f4a4511c85bb4d445491527db0f9ccddea3c24
sadatmalik <sadat.malik@me.com>
2023-10-24 22:31:34 +0100

    use http-client + observables for all crud operations

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -31,10 +31,13 @@ export class ReservationFormComponent implements OnInit{
 
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
-      let reservation = this.reservationService.getReservation(id);
-      if (reservation) {
-        this.reservationForm.patchValue(reservation);
-      }
+      this.reservationService.getReservation(id).subscribe(
+        reservation => {
+          if (reservation) {
+            this.reservationForm.patchValue(reservation);
+          }
+        }
+      );
     }
   }
 
@@ -45,10 +48,14 @@ export class ReservationFormComponent implements OnInit{
       let id = this.activatedRoute.snapshot.paramMap.get('id');
       if (id) {
         // Update
-        this.reservationService.updateReservation(id, reservation);
+        this.reservationService.updateReservation(id, reservation).subscribe(
+          () => console.log("Update request processed.")
+        );
       } else {
         // New
-        this.reservationService.addReservation(reservation);
+        this.reservationService.addReservation(reservation).subscribe(
+          () => console.log("Create request processed.")
+        );
       }
 
       this.router.navigate(['/list']);
--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -21,6 +21,8 @@ export class ReservationListComponent implements OnInit {
   }
 
   deleteReservation(id: string) {
-    this.reservationService.deleteReservation(id);
+    this.reservationService.deleteReservation(id).subscribe(
+      () => console.log("Delete request got processed.")
+    );
   }
 }
