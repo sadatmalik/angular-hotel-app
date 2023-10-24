f31bae931ca830a6b33f62167ac7e30a2c607097
sadatmalik <sadat.malik@me.com>
2023-10-24 21:45:57 +0100

    remove local-storage usage from reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,11 +8,6 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
-
   // CRUD
 
   getReservations(): Reservation[] {
@@ -27,18 +22,15 @@ export class ReservationService {
     reservation.id = Date.now().toString();
 
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
