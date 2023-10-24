f32b7f086b005258e974bacc02c65a9b98bc5930
sadatmalik <sadat.malik@me.com>
2023-10-24 21:58:14 +0100

    use http-client + observables to get reservations from mockoon back end

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -15,7 +15,9 @@ export class ReservationListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.reservations = this.reservationService.getReservations();
+    this.reservationService.getReservations().subscribe(
+      reservations => this.reservations = reservations
+    );
   }
 
   deleteReservation(id: string) {
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from "../models/reservation";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
+  private apiUrl = "http://localhost:3000";
+
   private reservations: Reservation[] = [];
+
+  constructor(private http: HttpClient) {
+  }
 
   // CRUD
 
-  getReservations(): Reservation[] {
-    return this.reservations;
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.apiUrl + "/reservations");
   }
 
   getReservation(id: string): Reservation | undefined {
